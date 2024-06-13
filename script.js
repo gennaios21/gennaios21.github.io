3a951266ed721f413b02b09679de6ee6ea4c8b48
Gennaios <gennaioslyk21@gmail.com>
2024-06-13 13:38:06 +0300

    fixed a bug where MO would not be printed

--- script.js
+++ script.js
@@ -111,10 +111,7 @@ function readSingleFile(evt) {
 document.getElementById('fileinput').addEventListener('change', readSingleFile, false);
 
 ////////////////////////////////////////////////////// calculate average 
-let count = 1
-
 function getMO() {
-
     let lessons1, lessons2, mo
 
     // ελεγχος αν εγινε import του WDP.xls
@@ -126,9 +123,9 @@ function getMO() {
 
     else {
         ////////// αριθμος περασμενων μαθηματων + τωρινος Μ.0
-        lessons1 = parseInt(Number(document.querySelectorAll('input')[3].value));
-        lessons2 = parseInt(Number(document.querySelectorAll('input')[4].value));
-        mo = (document.querySelectorAll('input')[5].value);
+        lessons1 = parseInt(Number(document.querySelectorAll('input')[2].value));
+        lessons2 = parseInt(Number(document.querySelectorAll('input')[3].value));
+        mo = (document.querySelectorAll('input')[4].value);
     }
 
     ////////// επιλογη σχολης
@@ -175,8 +172,8 @@ function getMO() {
 
     /// ελεγχοι για τιμες μεταξυ 5 και 10
     thesis = (thesis >= 5 && thesis <= 10) ? thesis : 0
-    lessons1 = (lessons1 >= 0 && lessons1 <= 39) ? lessons1 : window.alert("Αρ. μαθημάτων με συντελεστή 1.5 για πτυχίο: 27 μαθήματα κορμού και το πολύ 12 κατεύθυνσης")
-    lessons2 = (lessons2 >= 0 && lessons2 <= 27) ? lessons1 : window.alert("Αρ. μαθημάτων με συντελεστή 2 για πτυχίο: 11 μαθήματα κορμού, τουλάχιστον 4 κατεύθυνσης (4 υποχρεωτικά εργαστήρια) και το πολύ 16 κατεύθυνσης")
+    lessons1 = (lessons1 >= 0 && lessons1 <= 39) ? lessons1 : window.alert("Αρ. μαθημάτων με συντελεστή 1.5 για πτυχίο: 27 μαθήματα κορμού + το πολύ 12 κατεύθυνσης")
+    lessons2 = (lessons2 >= 0 && lessons2 <= 27) ? lessons2 : window.alert("Αρ. μαθημάτων με συντελεστή 2 για πτυχίο: 11 μαθήματα κορμού + τουλάχιστον 4 και το πολύ 16 κατεύθυνσης")
     mo = (mo >= 5 && mo <= 10) ? mo : window.alert("Πες την αλήθεια ωρέ, τι Μ.Ο έχεις;")
     
     ////////// νεος Μ.Ο.
@@ -185,27 +182,20 @@ function getMO() {
     
     /////////// Πληροφορια για τον νεο ΜΟ
     let show = document.createElement('div')
+    let text = show.innerText
     if(school == 'ece')
-        show.innerText = `(${count++}) Συγχαρητήρια μηχανευτή. Ο νέος Μ.Ο. σου είναι: ${new_mo}`
+        show.innerText = `Συγχαρητήρια μηχανευτή. Ο νέος Μ.Ο. σου είναι: ${new_mo}`
     else
-        show.innerText = `(${count++}) Συγχαρητήρια! Ο νέος Μ.Ο. σου είναι: ${new_mo}`
+        show.innerText = `Συγχαρητήρια! Ο νέος Μ.Ο. σου είναι: ${new_mo}`
 
     show.style.color = "blue";
     if(!isNaN(new_mo))
         document.querySelector('form').appendChild(show)
 
 }
-
 /////////////////////////////////////////////////////// προσθηκη και αφαιρεση μαθηματων
-let counter = 1
 function addLesson(k) {
-    counter++
-    if (counter >= 54) {
-        window.alert("Έφτασες τον μέγιστο αριθμό μαθημάτων")
-    }
-
-    else {
-        console.log(`Προστέθηκε μάθημα με συντελεστή: ${k}`)
+        // console.log(`Προστέθηκε μάθημα με συντελεστή: ${k}`)
 
         let input = document.createElement('input')
         input.type = "number"
@@ -242,4 +232,3 @@ function addLesson(k) {
             });
         }
     }
-}
